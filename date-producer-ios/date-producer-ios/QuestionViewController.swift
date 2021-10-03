2d543d2ec0d36c3e8f13d60113ff0d6a697783de
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-10-03 16:02:21 +0900

    [add]質問用のViewを追加

---
+++ date-producer-ios/date-producer-ios/QuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionViewController.swift
+//  date-producer-ios
+//
+//  Created by KaitoKudo on 2021/10/03.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
