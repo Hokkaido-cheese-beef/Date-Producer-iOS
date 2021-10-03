6a77b81414e43f014e39a0088b13cadf1350e910
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-10-03 23:14:03 +0900

    [fixix]オートレイアウトをつけて広告を表示

--- date-producer-ios/date-producer-ios/ResultViewController.swift
+++ date-producer-ios/date-producer-ios/ResultViewController.swift
@@ -5,14 +5,20 @@
 //  Created by KaitoKudo on 2021/10/03.
 //
 
+import GoogleMobileAds
 import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
     @IBOutlet weak var datePlanTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
     }
     
     @IBAction func storeDatePlan(_ sender: Any) {
