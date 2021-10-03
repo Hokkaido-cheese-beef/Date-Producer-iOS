de1537363dda504c1de4052eee2270eb7d3a58d9
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-10-03 19:01:51 +0900

    [fix]ファイル名修正

--- date-producer-ios/date-producer-ios/FirstQuestionViewController.swift
+++ date-producer-ios/date-producer-ios/FirstQuestionViewController.swift
@@ -8,13 +8,11 @@
 import GoogleMobileAds
 import UIKit
 
-class QuestionViewController: UIViewController {
+class FirstQuestionViewController: UIViewController {
     @IBOutlet weak var bannerView: GADBannerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Date Producer"
-        
         bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bannerView.rootViewController = self
         bannerView.load(GADRequest())
