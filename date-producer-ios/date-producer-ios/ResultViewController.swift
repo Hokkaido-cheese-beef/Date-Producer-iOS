7e3d4ebeb3a94987d594a2520f500051343718ec
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-10-03 19:37:27 +0900

    [add]結果画面を作成

---
+++ date-producer-ios/date-producer-ios/ResultViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultViewController.swift
+//  date-producer-ios
+//
+//  Created by KaitoKudo on 2021/10/03.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var datePlanTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func storeDatePlan(_ sender: Any) {
+    }
+    
+    @IBAction func regenerateDatePlan(_ sender: Any) {
+    }
+    
+    
+}
