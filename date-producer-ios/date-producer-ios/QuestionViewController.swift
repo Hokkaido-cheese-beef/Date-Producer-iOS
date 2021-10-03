439495e50a8d1bfab6a4c9d0b824501c611bb793
Kaito Kudou <kaitokudou2468110921@gmail.com>
2021-10-03 17:37:59 +0900

    [add]広告のViewを追加

--- date-producer-ios/date-producer-ios/QuestionViewController.swift
+++ date-producer-ios/date-producer-ios/QuestionViewController.swift
@@ -5,13 +5,19 @@
 //  Created by KaitoKudo on 2021/10/03.
 //
 
+import GoogleMobileAds
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    @IBOutlet weak var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        self.title = "Date Producer"
+        
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
     }
 
 }
